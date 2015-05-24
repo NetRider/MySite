c2402992d9a8f7c46bdbb48253995937309ed8cf
netrider91 <netrider91@gmail.com>
2015-05-24 17:06:05 +0200

    test commit

---
+++ Classes/Foundation/AbstractDataMapper.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matteopolsinelli
+ * Date: 24/05/15
+ * Time: 12:32
+ */
+
+namespace Foundation;
+
+abstract class AbstractDataMapper
+{
+    protected $adapter;
+    protected $entityTable;
+
+    public function __construct( Database $adapter) {
+        $this->adapter = $adapter;
+    }
+
+    public function getAdapter() {
+        return $this->adapter;
+    }
+
+    public function findByID($id)
+    {
+        $this->adapter->select($this->entityTable, array('id' => $id));
+
+        if (!$row = $this->adapter->fetch()) {
+            return null;
+        }
+
+        return $this->createEntity($row);
+    }
+
+    public function findAll(array $conditions = array())
+    {
+        $entities = array();
+        $this->adapter->select($this->entityTable, $conditions);
+        $rows = $this->adapter->fetchAll;
+
+        if($rows)
+        {
+            foreach($rows as $row)
+            {
+                $entities[] = $this->createEntity($row);
+            }
+        }
+
+        return $entities;
+    }
+    /*
+     * Questa funzione deve essere creata dai mappers concreti!
+     * Questo perché conoscono il tipo di modello
+     */
+    abstract protected function createEntity(array $row);
+}
---
+++ Classes/Foundation/UserMapper.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matteopolsinelli
+ * Date: 24/05/15
+ * Time: 13:01
+ */
+
+namespace Foundation;
+
+use Entity\User;
+
+class UserMapper extends AbstractDataMapper
+{
+    protected $entiyTable = "user";
+
+    public function insert(User $user)
+    {
+        $user->id = $this->adapter->insert($this->entiyTable, array("nickname"=>$user->getNickname(), "email"=>$user->getEmail(),
+                                        "password"=>$user->getPassword()));
+        return $user->id;
+    }
+
+    public function delete($id) {
+        if($id instanceof User) {
+            $id = $id->id;
+        }
+        return $this->adapter->delete($this->entityTable, array("id=$id"));
+    }
+
+    public function existUserName() {
+
+    }
+
+    protected function createEntity(array $row){
+
+        return new User($row["nickname"], $row["email"], $row["password"]);
+
+    }
+}
