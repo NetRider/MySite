9e6588895b651e373637c2d721c4cf0a99eb987b
Matteo <netrider91@gmail.com>
2015-11-05 22:14:29 +0100

    no message

--- Classes/Control/CommentController.php
+++ Classes/Control/CommentController.php
@@ -21,7 +21,7 @@ class CommentController extends Controller {
 		$databaseAdapter = new Database();
 		$commentMapper = new CommentMapper($databaseAdapter);
 		$session = Singleton::getInstance("Session");
-		$comment = new Comment($this->view->getCommentText(), date('o-m-d H:i:s'), $session->getUserId());
+		$comment = new CommentEntity($this->view->getCommentText(), date('o-m-d H:i:s'), $session->getUserId());
 
 		if($this->view->getArticleId() != "")
 			$this->view->responseAjaxCall($commentMapper->insertArticleComment($comment, $this->view->getArticleId()));
