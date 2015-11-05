bd0108ce2943d9f3780a1a41ffaa7ff2b8032a96
Matteo <netrider91@gmail.com>
2015-11-05 19:41:03 +0100

    no message

--- Classes/View/View.php
+++ Classes/View/View.php
@@ -7,10 +7,11 @@ abstract class View extends \Smarty {
 	public function __construct()
 	{
 		parent::__construct();
-	    $this->template_dir = '/Applications/XAMPP/xamppfiles/htdocs/MySite/Smarty_dir/templates';
-	    $this->compile_dir = '/Applications/XAMPP/xamppfiles/htdocs/MySite/Smarty_dir/templates_C';
-	    $this->config_dir = '/Applications/XAMPP/xamppfiles/htdocs/MySite/Smarty_dir/cache';
-	    $this->cache_dir = '/Applications/XAMPP/xamppfiles/htdocs/MySite/Smarty_dir/configs';
+		phpinfo();
+	    $this->template_dir = '/public_html/Smarty_dir/templates';
+	    $this->compile_dir = 'public_html/Smarty_dir/templates_C';
+	    $this->config_dir = 'public_html/Smarty_dir/cache';
+	    $this->cache_dir = 'public_html/Smarty_dir/configs';
 	    $this->caching = false;
 	}
 
